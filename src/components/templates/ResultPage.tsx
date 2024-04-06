7eaae4564aac929894f69e53a388014d90c6c044
Alireza Nikzad <alireza.nikzdd@gmail.com>
2024-04-06 21:52:15 +0330

    feat: add new page

--- src/components/templates/ResultPage.tsx
+++ src/components/templates/ResultPage.tsx
@@ -1,8 +1,22 @@
+import { useHumanBodyData } from "@/stores/human";
+import { Stack, Text } from "@chakra-ui/react";
+
 const ResultPage = () => {
+  const {
+    bodyData: { gender, height, result, weight },
+  } = useHumanBodyData();
+
   return (
-    <>
-      <h1>end page</h1>
-    </>
+    <Stack
+      alignItems={"center"}
+      spacing={10}
+      justifyContent={"center"}
+      h={"100svh"}
+    >
+      <Text>gender: {gender}</Text>
+      <Text>weight: {weight}cm</Text>
+      <Text>height: {height}kg</Text>
+    </Stack>
   );
 };
 
